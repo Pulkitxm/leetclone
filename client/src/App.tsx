3c216e021c848cfa93aee141d97fe169bbdf0a8e
Pulkit <kpulkit15234@gmail.com>
2024-05-02 22:12:02 +0530

    adding signin form ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import "./index.css";
 import Alert from "./components/Alert";
+import Login from "./pages/login";
+
 export default function App() {
   return (
     <div className="w-screen h-screen">
@@ -9,7 +12,7 @@ export default function App() {
       <Alert />
       <Routes>
         <Route path="/" element={<div>Home</div>} />
-        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<div>Register</div>} />
         <Route path="/:name" element={<div>Problems</div>} />
         <Route path="/*" element={<Navigate to={"/"} />} />
