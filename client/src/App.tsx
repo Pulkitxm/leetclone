b7ec0333b51d883d9f220aba56488176107167b6
Pulkit <kpulkit15234@gmail.com>
2024-05-02 21:41:26 +0530

    setting up routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./index.css";
 export default function App() {
   return (
     <div className="w-screen h-screen">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/:name" element={<div>Problems</div>} />
+        <Route path="/*" element={<Navigate to={"/"} />} />
+      </Routes>
     </div>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
     <App />
+  </BrowserRouter>
 );
