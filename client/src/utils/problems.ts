6d7c96afbf2f95b191b5da5e5f3d29b2dcf70c84
Pulkit <kpulkit15234@gmail.com>
2024-05-03 16:53:52 +0530

    adding /:id for problems

--- client/src/utils/problems.ts
+++ client/src/utils/problems.ts
@@ -5,3 +5,8 @@ export const getProblems = async() => {
     const res= await axios.get(baseUrl+"/api/_v1/codes");
     return res.data;
 };
+
+export const getProblem = async(id:string) => {
+    const res= await axios.get(baseUrl+"/api/_v1/codes/"+id);
+    return res.data;
+};
--- server/src/routes/code.ts
+++ server/src/routes/code.ts
@@ -5,34 +5,59 @@ import { PrismaClient } from "@prisma/client";
 const codeRouter = Router();
 
 codeRouter.get("/", async (req, res) => {
-    try {
-        const prisma= new PrismaClient();
-        const problems= await prisma.problem.findMany();
-        const allProbs= problems.map((prob)=> {
-            return {
-                name:prob.name,
-                topics:prob.topics,
-                difficulty:prob.difficulty,
-                id:prob.id
-            }
-        });
-        return res.status(200).json(allProbs);
-    } catch (error) {
-        res.status(500).send("Failed to fetch problems.");
-    }
+  try {
+    const prisma = new PrismaClient();
+    const problems = await prisma.problem.findMany();
+    const allProbs = problems.map((prob) => {
+      return {
+        name: prob.name,
+        topics: prob.topics,
+        difficulty: prob.difficulty,
+        id: prob.id,
+      };
+    });
+    return res.status(200).json(allProbs);
+  } catch (error) {
+    res.status(500).send("Failed to fetch problems.");
+  }
+});
+
+codeRouter.get("/:id", async (req, res) => {
+  try {
+    const prisma = new PrismaClient();
+    const problem = await prisma.problem.findUnique({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!problem) return res.status(404).send("Problem not found.");
+
+    return res.status(200).json({
+      name: problem.name,
+      topics: problem.topics,
+      difficulty: problem.difficulty,
+      id: problem.id,
+      constraints: problem.Constraints,
+      testCases: problem.testCases,
+      description: problem.description,
+    });
+  } catch (error) {
+    res.status(500).send("Failed to fetch problems.");
+  }
 });
 
 codeRouter.post("/submit", async (req, res) => {
-    const code = req.body.code;
-    const language = req.body.language;
+  const code = req.body.code;
+  const language = req.body.language;
 
-    try {
-        await client.lPush("problems", JSON.stringify({ code, language }));
-        res.status(200).send("Submission received and stored.");
-    } catch (error) {
-        console.error("Redis error:", error);
-        res.status(500).send("Failed to store submission.");
-    }
+  try {
+    await client.lPush("problems", JSON.stringify({ code, language }));
+    res.status(200).send("Submission received and stored.");
+  } catch (error) {
+    console.error("Redis error:", error);
+    res.status(500).send("Failed to store submission.");
+  }
 });
 
 export default codeRouter;
