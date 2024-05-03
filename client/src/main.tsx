02066110dcb0b6dd828f8762944c62b7e15d7a55
Pulkit <kpulkit15234@gmail.com>
2024-05-03 11:13:06 +0530

    adding react-cookie

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import { CookiesProvider } from "react-cookie";
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
-  </BrowserRouter>
+  <CookiesProvider defaultSetOptions={{ path: '/' }}>
+    <BrowserRouter>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </BrowserRouter>
+  </CookiesProvider>
 );
